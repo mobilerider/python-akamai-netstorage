9680e2144fae1d0931f2be85e575af1a4abbabe9
micho8402 <michel.perez8402@gmail.com>
2013-06-04 16:22:51 -0400

    Moves comment to the right line

--- netstorage/auth.py
+++ netstorage/auth.py
@@ -47,9 +47,9 @@ class AkamaiAuth(AuthBase):
 
     @property
     def __get_auth_sign(self):
-        # Version 5 - HMAC-SHA256([key], [data] + [sign-string])
         message = '%s%s' % (self.__auth_data_as_string, self.__get_sign_string)
 
+        # Version 5 - HMAC-SHA256([key], [data] + [sign-string])
         return hmac.new(self.key, msg=message, digestmod=hashlib.sha256).digest().encode('base64')
 
     def __set_headers(self, headers):
